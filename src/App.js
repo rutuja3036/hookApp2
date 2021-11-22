08ab2d04c67c9d668a14575996070f7d63db939f
rutuja <rutujaingale0230@example.com>
2021-11-22 16:36:50 +0530

    fetch the data and useState

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React ,{useReducer}from 'react';
+
 import CounterTwo from './components/reducerhook/CounterTwo';
 import CounterThree from './components/reducerhook/CounterThree';
 import CompA from './components/contextNreducer/compA';
 import CompB from './components/contextNreducer/CompB';
 import CompC from './components/contextNreducer/CompC';
+import DataFectching from './components/useReducercases/DataFetching';
 export const CountContext = React.createContext()
 const initialState = 0
 
-const reducer = (state,action) => {
-    switch(action){
-        case 'increment': return state+1
-        case 'decrement': return state-1
-        case 'reset': return initialState
-        default: return state
-    }
-}
 
 
 
 function App() {
   const [count,dispatch] = useReducer(reducer,initialState)
   return (
-    <CountContext.Provider 
-              value = {  
-                        {countState: count, countDispatch: dispatch} 
-                       }>
+    
     <div className="App">
-    Count in app.js is {count}
-      <CompA/>
-      <CompB/>
-      <CompC/>
+    <DataFectching/>
     </div>
-    </CountContext.Provider>
   );
 }
 
