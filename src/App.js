47506b257681b506397765cd33cba619c47d9a0a
rutuja <rutujaingale0230@example.com>
2021-11-22 10:23:05 +0530

    useContext hook to pass the data around

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import DataFetching from './components/hooks/DataFetching';
+import ComponentA from './components/contexthook/ComponentA';
+import React from 'react';
+export const UserContext = React.createContext()     //exporting a context
+export const BatchContext = React.createContext()
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <DataFetching/>np
+
+<UserContext.Provider value={'abdul'} >
+        <BatchContext.Provider value={'dxc batch 2'}>
+          <ComponentA />
+        </BatchContext.Provider>
+      </UserContext.Provider>
+    
+
+      
+      
     </div>
   );
 }
