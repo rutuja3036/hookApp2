7f35aca63143b5be836fd76143aa04ae4183149b
rutuja <rutujaingale0230@example.com>
2021-11-22 11:17:36 +0530

    passing additional value to action field

--- src/components/reducerhook/CounterTwo.js
+++ src/components/reducerhook/CounterTwo.js
@@ -1,41 +1,41 @@
 import React, { useReducer } from 'react'
 //demo about state as an object
-
 const initialState = {  //currently this object has only one partition called firstCounter
     firstCounter: 0
 }
 
 const reducer = (state, action) => {
     switch (action.type) {
-        case 'increment': return { firstCounter: state.firstCounter + 1 }
-        case 'decrement': return { firstCounter: state.firstCounter - 1 }
+        case 'increment': return { firstCounter: state.firstCounter + action.value }
+        case 'decrement': return { firstCounter: state.firstCounter - action.value}
         case 'reset': return initialState
         default: return state
     }
 }
-
 function CounterTwo() {
     const [count, dispatch] = useReducer(reducer, initialState)
-
     return (
         <div>
             <div> Count - {count.firstCounter}</div>
             <button
                 onClick={
-                    () => dispatch({ type: 'increment' })
-                }> Increment</button>
+                    () => dispatch({ type: 'decrement' , value:1})
+                }> Decrement</button>
+
+                <button
+                onClick={
+                    () => dispatch({ type: 'increment', value:5 })
+                }> Increment 5</button>
             <button
                 onClick={
-                    () => dispatch({ type: 'decrement' })
-                }> Decrement</button>
+                    () => dispatch({ type: 'decrement', value:5 })
+                }> Decrement 5 </button>
             <button
                 onClick={
                     () => dispatch({ type: 'reset' })
                 }> Reset</button>
-
-
         </div>
     )
 }
-
 export default CounterTwo
+
