369c359bbfb3ac2587ae1314a84e876e3c3566e2
rutuja <rutujaingale0230@example.com>
2021-11-22 11:14:03 +0530

    using an object as a state instead of an integer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import CounterOne from './components/reducerhook/CounterOne';
+import CounterTwo from './components/reducerhook/CounterTwo';
 
 
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
 
-<CounterOne/>     
+ <CounterTwo/>
     </div>
   );
 }
