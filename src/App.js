bcd8f7b6c35f4eb164329454f81e1a9794350583
rutuja <rutujaingale0230@example.com>
2021-11-22 16:17:25 +0530

    useContext and useReducer to share comp state with other comps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React ,{useReducer}from 'react';
 import CounterTwo from './components/reducerhook/CounterTwo';
 import CounterThree from './components/reducerhook/CounterThree';
+import CompA from './components/contextNreducer/compA';
+import CompB from './components/contextNreducer/CompB';
+import CompC from './components/contextNreducer/CompC';
+export const CountContext = React.createContext()
+const initialState = 0
 
+const reducer = (state,action) => {
+    switch(action){
+        case 'increment': return state+1
+        case 'decrement': return state-1
+        case 'reset': return initialState
+        default: return state
+    }
+}
 
 
 
 function App() {
+  const [count,dispatch] = useReducer(reducer,initialState)
   return (
+    <CountContext.Provider 
+              value = {  
+                        {countState: count, countDispatch: dispatch} 
+                       }>
     <div className="App">
-
-<CounterThree/>
+    Count in app.js is {count}
+      <CompA/>
+      <CompB/>
+      <CompC/>
     </div>
+    </CountContext.Provider>
   );
 }
 
---
+++ src/components/contextNreducer/CompB.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CompD from './CompD'
+
+
+function CompB() {
+    return (
+        <div>
+            <CompD/>
+            
+        </div>
+    )
+}
+
+export default CompB
