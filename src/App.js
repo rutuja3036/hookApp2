a8b4292d55402d1e6c693438d6288a213a8a2ebf
rutuja <rutujaingale0230@example.com>
2021-11-24 10:37:27 +0530

    life b4 higher order components -- part2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import ClickCounter from './components/higherordercomponents/ClickCounter';
+import HoverCounter from './components/higherordercomponents/HoverCounter';
 
 
 
@@ -11,7 +12,7 @@ function App() {
   return (
     
     <div className="App">
-
+<HoverCounter/>
     <ClickCounter/>
     </div>
   );
