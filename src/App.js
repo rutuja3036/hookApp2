970df9ab315a6274d58f39b3547b511e54382124
rutuja <rutujaingale0230@example.com>
2021-11-26 11:53:04 +0530

    passing values to the custom hoo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import './App.css';
 import React from 'react';
 import Counter1 from './customhooks/effect/functionality/Counter1';
 import Counter2 from './customhooks/effect/functionality/Counter2';
-
-
-
-
 function App() {
   
   return (
--- src/customhooks/effect/functionality/Counter2.js
+++ src/customhooks/effect/functionality/Counter2.js
@@ -3,8 +3,7 @@ import useCounter from './useCounter'
 
 
 function Counter2() {
-    const [count, increment,decrement,reset] = useCounter()
-
+    const [count, increment,decrement,reset] = useCounter(10,5)
     return (
         <div>
             <h2> Count = {count}</h2>
