7dd36a4c249ca0151a9cfb88f661e4b1ccee7098
rutuja <rutujaingale0230@example.com>
2021-11-26 11:00:57 +0530

    custom hook doc title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import React from 'react';
 import ClickCounter from './components/higherordercomponents/ClickCounter';
 import HoverCounter from './components/higherordercomponents/HoverCounter';
+import DocTitle from './customhooks/DocTitle';
+import DocTitle2 from './customhooks/DocTitle2';
 
 
 
@@ -12,8 +14,8 @@ function App() {
   return (
     
     <div className="App">
-<HoverCounter/>
-    <ClickCounter/>
+<DocTitle/>
+<DocTitle2/>
     </div>
   );
 }
